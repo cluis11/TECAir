600e552d1f738447c3a85f8863961fd12f9f714a
cluis11 <cluis.9405@gmail.com>
2026-05-23 00:54:34 -0600

    gestion de ruta v1

--- TECAirAPI/Data/Repositories/Interfaces/IRutaRepository.cs
+++ TECAirAPI/Data/Repositories/Interfaces/IRutaRepository.cs
@@ -8,9 +8,11 @@ public interface IRutaRepository
     Task<IEnumerable<Ruta>> GetAll();
     Task<IEnumerable<Ruta>> GetById(int idRuta);
     Task<IEnumerable<Ruta>> GetRutas(int origen, int destino);
+    Task<IEnumerable<RutaResultadoDTO>> GetAllParaPromo();
     Task<Ruta> PostRuta(Ruta ruta);
     Task<Ruta> PutRuta(int id_ruta, Ruta ruta);
     Task<bool> DeleteRuta(int idRuta);
     Task<IEnumerable<Vuelo>> GetVuelo(int idVuelo);
-    Task<IEnumerable<RutaResultadoDTO>> GetAllParaPromo();
+    Task<Vuelo> InsertVuelo(Vuelo vuelo);
+    Task<bool> DeleteVuelo(int idVuelo);
 }
